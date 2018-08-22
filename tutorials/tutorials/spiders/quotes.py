445d03b6535653e87e788f78765038759839288b
jvitorfranca <vitoorbr@gmail.com>
2018-08-22 09:51:57 -0300

    parsing following links

--- tutorials/tutorials/spiders/quotes.py
+++ tutorials/tutorials/spiders/quotes.py
@@ -14,7 +14,7 @@ class QuotesSpider(scrapy.Spider):
         for quote in response.css('div.quote'):
             item = {
                 'author_name': quote.css('small.author::text').extract(),
-                'text': quote.css('span.text::text').extract_first().strip(u'\u201c\u201d\u00e9'),
+                'text': quote.css('span.text::text').extract_first(),
                 'tags': quote.css('a.tag::text').extract(),
             }
             yield item
